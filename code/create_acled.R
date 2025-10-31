2a05fa68f41430a758fcbdcf9f3b43c5708c7c9e
Kristine Helskog <krihel@prio.org>
2025-10-31 14:13:52 +0100

    Merge ACLED

--- code/create_acled.R
+++ code/create_acled.R
@@ -8,23 +8,23 @@ library(tidyverse)
 
 # Africa 
 
-Africa <- read_xlsx("raw data/Africa_lagged_data_up_to-2024-10-24.xlsx")
+Africa <- read_xlsx("raw_data/Africa_lagged_data_up_to-2024-10-24.xlsx")
 
 # Middle East 
 
-ME <- read_xlsx("raw data/Middle-East_lagged_data_up_to-2024-10-24.xlsx")
+ME <- read_xlsx("raw_data/Middle-East_lagged_data_up_to-2024-10-24.xlsx")
 
 # Latin America 
 
-LA <- read_xlsx("raw data/Latin-America-the-Caribbean_lagged_data_up_to-2024-10-24.xlsx")
+LA <- read_xlsx("raw_data/Latin-America-the-Caribbean_lagged_data_up_to-2024-10-24.xlsx")
 
 # Europe and Central Asia
 
-ECA <- read_xlsx("raw data/Europe-Central-Asia_lagged_data_up_to-2024-10-24.xlsx")
+ECA <- read_xlsx("raw_data/Europe-Central-Asia_lagged_data_up_to-2024-10-24.xlsx")
 
 # Asia and Pacific 
 
-AP <- read_xlsx("raw data/Asia-Pacific_lagged_data_up_to-2024-10-24.xlsx")
+AP <- read_xlsx("raw_data/Asia-Pacific_lagged_data_up_to-2024-10-24.xlsx")
 
 
 # -------------------------------------------------------
